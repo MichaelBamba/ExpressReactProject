afe2f99712ef0c334029e382c7ded63cffb0086b
MichaelBamba <mjbamba2@gmail.com>
2020-04-02 15:15:49 -0400

    created first model and save progress

---
+++ models/post.js
@@ -0,0 +1,29 @@
+const db = require("./con.js");
+
+class postReview {
+  constructor(id, user_id, post) {
+    this.id = id;
+    this.user_id;
+    this.post;
+  }
+  static async getpost() {
+    try {
+      const respose = await db.any(`SELECT * FROM blogPosts;`);
+      return respose;
+    } catch (error) {
+      console.error("ERROR", error);
+      return error;
+    }
+  }
+  static async getpostId() {
+    try {
+      const response = await db.any(`SELECT * FROM blogPosts WHERE id = 1;`);
+      return response;
+    } catch (error) {
+      console.error("ERROR", error);
+      return error;
+    }
+  }
+}
+
+module.exports = postReview;
