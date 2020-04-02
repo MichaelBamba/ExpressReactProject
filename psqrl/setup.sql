a10e6365c171c82b528e7f49b4815f72f3919097
MichaelBamba <mjbamba2@gmail.com>
2020-04-02 11:41:59 -0400

    had to remember how to use psql making a basic outline for the database to be created

---
+++ psqrl/setup.sql
@@ -0,0 +1,14 @@
+create database pandemicBlog
+
+create table users ( user_id serial primary key, 
+username varchar,
+email varchar);
+
+create table blogPosts(blod_id serial primary key,
+ user_id integer References users(user_id),
+ post varchar,
+
+
+create table Comments (
+    comment_id serial primary key
+)
