8f4218b40233e6d13e4cc5247ab10703d5349190
MichaelBamba <mjbamba2@gmail.com>
2020-04-03 16:11:38 -0400

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -10,6 +11,7 @@ var postRouter = require("./routes/post");
 var app = express();
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
